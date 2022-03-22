1b0b86ae2d7a8b9d988aba351c2227464f57e779
Magali <magali.thuaire@gmail.com>
2022-03-22 13:13:13 +0100

    ✨ [CLIENT] Added entity Trick

---
+++ src/Entity/Trick.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: TrickRepository::class)]
+class Trick
+{
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private $title;
+
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     */
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private $slug;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    #[ORM\Column(type: 'integer')]
+    private $category;
+
+    public const CATEGORY = [
+        0 => 'grab',
+        1 => 'spin',
+        2 => 'flip and inverted rotation'
+    ];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = strtolower($title);
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?int
+    {
+        return $this->category;
+    }
+
+    public function setCategory(int $category): self
+    {
+
+        $this->category = $category;
+
+        return $this;
+    }
+}
