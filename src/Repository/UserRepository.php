303419513fa427ff7c2ca6d8fe64ca42a7411d1f
Magali <magali.thuaire@gmail.com>
2022-03-30 20:55:36 +0200

    ✨ [AUTH] Added Forgot Password

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,8 +4,6 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
